42a46dff4b2b967a9c89910fc55590b0be0831b5
Ricardo Leon <ricardo_leon1@usmp.pe>
2020-07-05 16:36:01 -0500

    preparando datos de entrenamiento

---
+++ entrenamientoRF.py
@@ -0,0 +1,26 @@
+import cv2  #pip install opencv-python
+import os 
+import numpy as np #pip install numpy
+
+dataPath = 'D:/Ricardo/Documentos/reconocimiento_facial/data'
+peopleList = os.listdir(dataPath)
+print('Lista de personas: ', peopleList)
+
+labels = []
+facesData = []
+label = 0
+
+for nameDir in peopleList:
+    personPath = dataPath + '/' + nameDir
+    print('Leyendo las imagenes')
+
+    for fileName in os.listdir(personPath):
+        print('Rostros: ', nameDir + '/' + fileName)
+        labels.append(label)
+        facesData.append(cv2.imread(personPath + '/' + fileName, 0))
+        image = cv2.imread(personPath + '/' + fileName, 0)
+        #cv2.imshow('image', image)
+        #cv2.waitKey(10)
+    label = label + 1
+
+print('labels', labels)
