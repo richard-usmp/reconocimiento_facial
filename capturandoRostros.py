a602e0f42fe881b45ddcf52ab95cc63548d2a406
Ricardo Leon <ricardo_leon1@usmp.pe>
2020-07-12 13:15:56 -0500

    Empezando pagina web

--- capturandoRostros.py
+++ capturandoRostros.py
@@ -2,41 +2,50 @@ import cv2  #pip install opencv-python
 import os 
 import imutils #pip install imutils
 
-personName = 'Aaron'
-personName = 'Ricardo'
-dataPath = 'D:/Ricardo/Documents/reconocimiento_facial/data'
-personPath = dataPath + '/' + personName
-#print(personPath)
-if not os.path.exists(personPath):
-    print('Carpeta creada: ', personPath)
-    os.makedirs(personPath)
+class capturandoRostros:
+    persona = ""
+    edad = 0
+    video = ""
+    
+    def __init__(self,nombre,apellido,edad,video):
+        self.persona=nombre+" "+ apellido
+        self.edad = edad
+        self.video = video
+    
+    personName = persona
+    dataPath = 'D:/Ricardo/Documents/reconocimiento_facial/data'
+    personPath = dataPath + '/' + personName
+    #print(personPath)
+    if not os.path.exists(personPath):
+        print('Carpeta creada: ', personPath)
+        os.makedirs(personPath)
 
-#cap = cv2.VideoCapture('Aaron.mp4')
-cap = cv2.VideoCapture('Ricardo.mp4')
+    #cap = cv2.VideoCapture('Aaron.mp4')
+    cap = cv2.VideoCapture(video)
 
-faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-c = 0
+    faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    c = 0
 
-while(True):
-    ret, frame = cap.read()
-    if ret == False : break
-    frame = imutils.resize(frame, width=640)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    auxFrame = frame.copy()
+    while(True):
+        ret, frame = cap.read()
+        if ret == False : break
+        frame = imutils.resize(frame, width=640)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        auxFrame = frame.copy()
 
-    faces = faceClassif.detectMultiScale(gray, 1.3, 5)
+        faces = faceClassif.detectMultiScale(gray, 1.3, 5)
 
-    for(x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
-        rostro = auxFrame[y:y+h, x: x+w]
-        rostro = cv2.resize(rostro, (150, 150), interpolation=cv2.INTER_CUBIC)
-        cv2.imwrite(personPath + '/rostro_{}.jpg'.format(c), rostro)
-        c=c+1
-    cv2.imshow('frame', frame )
+        for(x,y,w,h) in faces:
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+            rostro = auxFrame[y:y+h, x: x+w]
+            rostro = cv2.resize(rostro, (150, 150), interpolation=cv2.INTER_CUBIC)
+            cv2.imwrite(personPath + '/rostro_{}.jpg'.format(c), rostro)
+            c=c+1
+        cv2.imshow('frame', frame )
 
-    k= cv2.waitKey(1)
-    if k==27 or c >= 300:
-        break
+        k= cv2.waitKey(1)
+        if k==27 or c >= 300:
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
---
+++ index.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+import capturandoRostros as CP
+
+app = Flask (__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/perdido_alguien')
+def perdido_alguien():
+    return render_template('perdido_alguien.html')
+
+@app.route('/personas_desaparecidas')
+def personas_desaparecidas():
+    return render_template('personas_desaparecidas.html')
+
+@app.route('/pruebas')
+def pruebas():
+    return render_template('pruebas.html')
+
+@app.route('/formulario',methods=['POST'])
+def formulario():
+        if request.method == 'POST':
+            nombre = request.form['nombre']
+            apellido = request.form['apellido']
+            edad = request.form['edad']
+            video = request.form['video']
+            print(nombre,apellido,edad)
+            CP.capturandoRostros(nombre,apellido,edad,video)
+            print('received')    
+
+if __name__ == '__main__':
+    app.run(debug=True)
