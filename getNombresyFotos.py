28de6d2a0794ed82e4fdd26c1b11d92bad25a1f3
Ricardo Leon <ricardo_leon1@usmp.pe>
2020-07-29 19:00:45 -0500

    personas desaparecidas en progreso

---
+++ getNombresyFotos.py
@@ -0,0 +1,18 @@
+import os 
+import numpy as np #pip install numpy
+
+dataPath = 'D:/Ricardo/Documentos/reconocimiento_facial/data'
+peopleList = os.listdir(dataPath)
+print('Lista de personas: ', peopleList)
+
+def getNombres():
+    return peopleList
+
+def getImage():
+    image = []
+    for nameDir in peopleList:
+        personPath = dataPath + '/' + nameDir
+        for fileName in os.listdir(personPath):
+            image_url = personPath + '/' +fileName
+        image.append(image_url)
+    return image
--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 import capturandoRostros as CP
+import getNombresyFotos as getNF
 import shutil
 import os
 import entrenamientoRF
-app = Flask (__name__)
+app = Flask (__name__, static_url_path='/data')
 
 @app.route('/')
 def home():
@@ -16,7 +17,15 @@ def perdido_alguien():
 
 @app.route('/personas_desaparecidas')
 def personas_desaparecidas():
-    return render_template('personas_desaparecidas.html')
+    arrayNom =[]
+    fotox = []
+    for i in range(len(getNF.getNombres())):
+        arrayNom.append(getNF.getNombres()[i])
+    
+    for i in range(len(getNF.getImage())):
+        fotox.append(getNF.getImage()[i])
+        print(fotox)
+    return render_template('personas_desaparecidas.html', arrayNom_e = arrayNom, foto = fotox)
 
 @app.route('/tests')
 def pruebas():
